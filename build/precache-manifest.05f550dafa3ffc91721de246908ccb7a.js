12c9c00409e2d7c4577ba22d8941ba476fea98eb
Jenifer-Arockiasamy <jenifer3193@gmail.com>
2019-06-26 18:40:47 +0530

    Update index.html

---
+++ build/precache-manifest.05f550dafa3ffc91721de246908ccb7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca706476bb2ec8bd28c8b881fa80084",
+    "url": "./index.html"
+  },
+  {
+    "revision": "37d4bb6dfa5338e03176",
+    "url": "./static/css/main.39ba1cfb.chunk.css"
+  },
+  {
+    "revision": "e95c82fc1b7f6c0fc065",
+    "url": "./static/js/2.7584419e.chunk.js"
+  },
+  {
+    "revision": "37d4bb6dfa5338e03176",
+    "url": "./static/js/main.495fd45f.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
